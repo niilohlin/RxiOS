40f44e2383178e418b0f23df01a08eccf89d7419
Niil Ohlin <niilohlin@gmail.com>
2017-03-29 18:01:18 +0200

    add connectionManager

---
+++ RxiOS/Proxies/DelegateProxy.cs
@@ -0,0 +1,13 @@
+﻿namespace UIKit.Reactive.Proxies
+{
+    public class DelegateProxy
+    {
+
+        private object _parent;
+
+        public DelegateProxy(object parent)
+        {
+            parent = parent;
+        }
+    }
+}
---
+++ RxiOSExample/ConnectionManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+namespace RxiOSExample
+{
+    public class ConnectionManager
+    {
+        public IObservable<HttpResponseMessage> GetGithub()
+        {
+            var client = new HttpClient();
+            return Observable.Create<HttpResponseMessage>(async observer =>
+            {
+                var response = await client.GetAsync("https://api.github.com/");
+                observer.OnNext(response);
+                observer.OnCompleted();
+                return Disposable.Empty;
+            });
+        }
+
+    }
+}
