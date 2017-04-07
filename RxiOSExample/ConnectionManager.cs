30c74addfd19341c2b614d95d5afedcc5280baba
Niil Ohlin <niilohlin@gmail.com>
2017-04-07 11:15:57 +0200

    add json nuget package

--- RxiOSExample/ConnectionManager.cs
+++ RxiOSExample/ConnectionManager.cs
@@ -1,24 +1,52 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace RxiOSExample
 {
     public class ConnectionManager
     {
-        public IObservable<HttpResponseMessage> Login(string username, string password)
+        private IObservable<T> Get<T>(Uri uri)
         {
-            return Observable.FromAsync(async observer =>
-            {
-                using (var handler = new HttpClientHandler { Credentials = new NetworkCredential(username, password), UseDefaultCredentials = true})
-                using (var client = new HttpClient(handler))
-                {
-                    return await client.GetAsync("https://api.github.com/user/", observer);
-                }
-            });
+            return
+                Observable.Using(
+                    () =>
+                        new HttpClient(new HttpClientHandler
+                        {
+                            Credentials = new NetworkCredential("", ""),
+                            UseDefaultCredentials = true
+                        }),
+                    client => client.GetStreamAsync(uri)
+                        .ToObservable()
+                        .Select(
+                            x =>
+                                Observable.FromAsync(
+                                        () => new StreamReader(x, Encoding.UTF8, false, 1024, true).ReadLineAsync())
+                                    .Repeat())
+                        .Concat()
+                        .Where(x => !string.IsNullOrWhiteSpace(x))
+                        .Select(JsonConvert.DeserializeObject<T>));
+                
+            //return Observable.FromAsync(async cancellationToken =>
+            //{
+            //    using (var handler = new HttpClientHandler { Credentials = new NetworkCredential(username, password), UseDefaultCredentials = true})
+            //    using (var client = new HttpClient(handler))
+            //    {
+            //        var result = await client.GetAsync(, cancellationToken);
+            //        if (!result.IsSuccessStatusCode)
+            //        {
+            //        }
+                    
+            //        return result;
+            //    }
+            //});
         }
     }
 }
