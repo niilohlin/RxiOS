1ab0b662787595e16bb0e9a3a87da42e45a2e4fc
Niil Ohlin <niilohlin@gmail.com>
2017-05-15 17:30:00 +0200

    change viewmodel to be pure

--- RxiOSExample/LoginViewModel.cs
+++ RxiOSExample/LoginViewModel.cs
@@ -9,36 +9,65 @@ using CoreAnimation;
 
 namespace RxiOSExample
 {
-    public class LoginViewModel
+    public interface ILoginViewModelInputs
     {
-        public BehaviorSubject<string> Username = new BehaviorSubject<string>("");
-        public BehaviorSubject<string> Password = new BehaviorSubject<string>("");
-        public IObservable<string> LoginButtonTitle = Observable.Return("Login");
-        public IObservable<string> UsernamePlaceholder = Observable.Return("Username");
-        public IObservable<string> PasswordPlaceholder = Observable.Return("Password");
-        public IObservable<bool> LoginButtonEnabled;
-        public BehaviorSubject<bool> ActivityIndicatorViewShowing = new BehaviorSubject<bool>(false);
-        public Subject<Exception> ErrorOccured = new Subject<Exception>();
+        IObservable<string> Username { get; }
+        IObservable<string> Password { get; }
+        IObservable<Unit> ButtonTap { get; }
+    }
+    public interface ILoginViewModelOutputs
+    {
+        IObservable<string> LoginButtonTitle { get; }
+        IObservable<string> UsernamePlaceholder { get; }
+        IObservable<string> PasswordPlaceholder { get; }
+        IObservable<bool> LoginButtonEnabled { get; }
+        IObservable<bool> ActivityIndicatorViewShowing { get;  }
+        IObservable<Exception> ErrorOccured { get; }
+        IObservable<Unit> Login { get; }
+    }
+
+    public interface ILoginViewModel
+    {
+        ILoginViewModelInputs Inputs { get; }
+        ILoginViewModelOutputs Outputst { get; }
+
+    }
+
+    public class LoginViewModel: ILoginViewModel, ILoginViewModelInputs, ILoginViewModelOutputs
+    {
+        public IObservable<string> Username { get; set; }
+        public IObservable<string> Password { get; set; }
+        public IObservable<Unit> ButtonTap { get; }
+        public IObservable<string> LoginButtonTitle { get; }
+        public IObservable<string> UsernamePlaceholder { get; }
+        public IObservable<string> PasswordPlaceholder { get; }
+        public IObservable<bool> LoginButtonEnabled { get; }
+        public IObservable<bool> ActivityIndicatorViewShowing { get; }
+        public IObservable<Exception> ErrorOccured { get; }
+        public ILoginViewModelInputs Inputs => this;
+        public ILoginViewModelOutputs Outputst => this;
 
         public LoginViewModel()
         {
             LoginButtonEnabled = Username
                 .CombineLatest(Password, (username, password) => username != "" && password != "")
                 .CombineLatest(ActivityIndicatorViewShowing, (enabled, showing) => enabled && !showing);
+
+            Login
+
+                ActivityIndicatorViewShowing.OnNext(true);
+                if (Username.Value == "Asdf" && Password.Value == "fdsa")
+                {
+                    return Observable.Return(Unit.Default)
+                        .DelaySubscription(TimeSpan.FromSeconds(3))
+                        .Finally(() => ActivityIndicatorViewShowing.OnNext(false));
+                }
+                ErrorOccured.OnNext(new Exception("Wrong username or password"));
+                ActivityIndicatorViewShowing.OnNext(false);
+                return Observable.Never<Unit>();
         }
 
-        public IObservable<Unit> Login()
-        {
-            ActivityIndicatorViewShowing.OnNext(true);
-            if (Username.Value == "Asdf" && Password.Value == "fdsa")
-            {
-                return Observable.Return(Unit.Default)
-                    .DelaySubscription(TimeSpan.FromSeconds(3))
-                    .Finally(() => ActivityIndicatorViewShowing.OnNext(false));
-            }
-            ErrorOccured.OnNext(new Exception("Wrong username or password"));
-            ActivityIndicatorViewShowing.OnNext(false);
-            return Observable.Never<Unit>();
-        }
+        public IObservable<Unit> Login { get; }
+
     }
 }
